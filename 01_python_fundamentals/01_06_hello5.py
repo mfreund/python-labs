3134c16dae25df8ed11ea5d450e0d1d81adfbaf6
Cookiehook <shaun.harrison@live.co.uk>
2020-05-14 20:45:04 +0100

    Introduced for loop

--- 01_python_fundamentals/01_06_hello5.py
+++ 01_python_fundamentals/01_06_hello5.py
@@ -5,8 +5,7 @@ Write the necessary code to display "Hello World!" 5 times.
 
 '''
 
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
+
+if __name__ == '__main__':
+    for number in range(5):
+        print("Hello World! " + str(number))
