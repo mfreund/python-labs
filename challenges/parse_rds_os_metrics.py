2739cca2ea79d8572946e13cc1bc414f61308c23
Cookiehook <shaun.harrison@live.co.uk>
2020-06-18 20:41:36 +0100

    Challenge #1

---
+++ challenges/parse_rds_os_metrics.py
@@ -0,0 +1,18 @@
+"""
+Requirements:
+Parse the input_data/log-events-viewer-result.csv file to provide summary data for:
+    - Date & Timestamps of first and last entries.
+    - Total CPU Utilisation per 15minutes, 30minutes, 60minutes
+    - Total diskIO read / write (as separate figures) for device rdsdev
+    - Average memoryUsedPC for each process in process list, in the last hour, grouped by process name
+
+Hints:
+    - This is a CSV file, with columns for timestamp and log message
+    - You may use pure python (no pip installations required), or external packages if you wish
+    - Try to understand one line of the log file before diving into all 300+ rows
+    - If you can't satisfy every
+"""
+
+
+if __name__ == '__main__':
+    pass
